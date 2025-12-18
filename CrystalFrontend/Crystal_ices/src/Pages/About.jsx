4a6fdc30e380fc9c7a4936721f077e38564eddff
Favour <georgechiamaka02@gmail.com>
2025-12-18 16:17:32 +0100

    created layout

--- CrystalFrontend/Crystal_ices/src/Pages/About.jsx
+++ CrystalFrontend/Crystal_ices/src/Pages/About.jsx
@@ -1,14 +1,14 @@
 import Navbar from "../Shared/Navbar";
 
 const About = () => {
-    return ( 
-        <div>
-         <Navbar />
-           <div className="bg-red-500 text-white">
+  return (
+    <div>
+      <Navbar />
+      <div className="bg-red-500 text-white">
         <p>about page</p>
       </div>
-        </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default About;
