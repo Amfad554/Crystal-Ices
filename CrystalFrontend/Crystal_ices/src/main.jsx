d0ea55c35b487bd47b47debbf7de223f27c863d8
Favour <georgechiamaka02@gmail.com>
2025-12-18 20:09:16 +0100

    fix: total reset of file casing

--- CrystalFrontend/Crystal_ices/src/main.jsx
+++ CrystalFrontend/Crystal_ices/src/main.jsx
@@ -6,12 +6,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
 import Contact from './Pages/Contact.jsx';
-import Services from './Pages/services.jsx';
+
 import Project from './Pages/Project.jsx';
 import Catalogue from './Pages/Catalogue.jsx';
 import News from './Pages/News.jsx';
 import Careers from './Pages/Careers.jsx';
 import Privacy from './Pages/Privacy.jsx';
+import Services from './Pages/Services.jsx';
+
 
 const router = createBrowserRouter([
   {
