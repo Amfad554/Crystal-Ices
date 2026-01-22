6c967cacbbe5981373ae9aee50e24b2bf00b710c
Favour <georgechiamaka02@gmail.com>
2026-01-22 17:24:53 +0100

    added image to about page

--- CrystalFrontend/Crystal_ices/src/Pages/About.jsx
+++ CrystalFrontend/Crystal_ices/src/Pages/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
             <img
               src="https://res.cloudinary.com/debum5mhu/image/upload/v1769094466/ceo_q0gojr.jpg"
               alt="CEO"
-              className="w-[100%] h-[80%] object-cover object-top hover:scale-105 transition-transform duration-500"
+              className="w-full h-full object-contain bg-slate-50 hover:scale-105 transition-transform duration-500"
             />
           </div>
         </section>
